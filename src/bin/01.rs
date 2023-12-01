a1a7dbb58a3c1be3e59d7d302179d048106ce591
Laurent <haarlaurent67@hotmail.fr>
2023-12-01 14:11:34 +0100

    smaller replacements

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -25,16 +25,16 @@ pub fn part_one(input: &str) -> Option<u32> {
 pub fn part_two(input: &str) -> Option<u32> {
     // replacing like this works, but why?
     let splitted = input
-        .replace("one", "one1one")
-        .replace("two", "two2two")
-        .replace("three", "three3three")
-        .replace("four", "four4four")
-        .replace("five", "five5five")
-        .replace("six", "six6six")
-        .replace("seven", "seven7seven")
-        .replace("eight", "eight8eight")
-        .replace("nine", "nine9nine")
-        .replace("zero", "zero0zero");
+        .replace("one", "o1e")
+        .replace("two", "t2o")
+        .replace("three", "t3e")
+        .replace("four", "f4r")
+        .replace("five", "f5e")
+        .replace("six", "s6x")
+        .replace("seven", "s7n")
+        .replace("eight", "e8t")
+        .replace("nine", "n9e")
+        .replace("zero", "z0o");
     let splitted = splitted.lines();
     let mut num_array: Vec<char> = Vec::new();
     let mut calibrations_numbers: Vec<u32> = Vec::new();
